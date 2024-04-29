948b6c64671b96b5a1ec4562fe9f5bd3170e39b7
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-04-29 15:05:19 +0300

    feat: install styles plugins and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let mode = 'development';
 if (process.env.NODE_ENV === 'production') {
@@ -11,8 +12,33 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './index.html'
     }),
+    new MiniCssExtractPlugin(),
   ],
   module: {
-    rules: [],
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          (mode === 'development') ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
   },
 }
