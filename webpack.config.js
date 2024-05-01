2872b09c94f115b5ed1443ceda11cdb74327d1a3
ArishaSuperNyasha <126005088+ArishaSuperNyasha@users.noreply.github.com>
2024-05-01 16:19:20 +0500

    feat: 1_08-configure bundler
    * feat: add basic webpack settings
    
    * feat: add html-webpack-plugin and html template
    
    * feat: install styles plugins and loaders
    
    * feat: add styles compilation with hash
    
    * feat: add images loader
    
    * feat: add html loader
    
    * feat: add cleaning before each new compilation
    
    * feat: add font loader
    
    * feat: add svg-sprite-loader
    
    * feat: add module-alias plugin
    
    * feat: add post-css options and edit assets output name

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+
+module.exports = {
+  mode: mode,
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      "@shared": path.resolve(__dirname, 'src/shared/'),
+      "@core": path.resolve(__dirname, 'src/modules/core/'),
+      "@sharedStyles": path.resolve(__dirname, 'src/shared/styles'),
+    },
+  },
+  output: {
+    assetModuleFilename: 'assets/[name][hash][ext][query]',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          (mode === 'development') ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      stage: 3, 
+                      features: {
+                        'custom-properties': false,
+                      },
+                      autoprefixer: { grid: true },
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        loader: 'asset/resource',
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-sprite-loader',
+          }
+        ]
+      }
+    ],
+  },
+}
