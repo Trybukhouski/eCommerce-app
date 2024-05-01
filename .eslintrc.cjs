e6c67cb5db116d1ed6adb7d2c5d7cd9191dbdc32
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-05-01 17:10:27 +0300

    feat: configure eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,25 @@
 module.exports = {
   extends: [
+    "airbnb-base",
     "airbnb-typescript-prettier"
   ],
   parserOptions: {
-    sourceType: 'module',
-    project: 'tsconfig.json',
+    sourceType: "module",
+    project: "tsconfig.json",
     tsconfigRootDir: __dirname,
     warnOnUnsupportedTypeScriptVersion: false,
   },
+  plugins: [
+    "prettier"
+  ],
   rules: {
     "max-lines-per-function": ["error", 40],
+    
+    "import/no-unresolved": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "class-methods-use-this": "off"
+  },
+  settings: {
   }
 }
