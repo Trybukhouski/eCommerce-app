55cc9929e28b5db2439bd55b862c7456cc22a868
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-05-02 14:08:38 +0300

    feat: install jest

---
+++ jest.config.mjs
@@ -0,0 +1,21 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+  maxWorkers: "50%",
+
+  // A preset that is used as a base for Jest's configuration
+  preset: "ts-jest",
+
+  // The root directory that Jest should scan for tests and modules within
+  rootDir: './src',
+};
+
+export default config;
