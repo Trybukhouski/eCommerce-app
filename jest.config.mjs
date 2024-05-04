507036a1a7b8863eb007a30b383c0c692d1d1ba1
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-05-04 15:38:55 +0300

    feat: install identity-obj-proxy for styles in tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -16,6 +16,12 @@ const config = {
 
   // The root directory that Jest should scan for tests and modules within
   rootDir: './src',
+  
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
 };
 
 export default config;
