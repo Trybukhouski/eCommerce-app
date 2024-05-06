4f5d32ffae97346915886b2dc1fe754fb1877987
Trybukhouski <71073153+Trybukhouski@users.noreply.github.com>
2024-05-06 14:05:52 +0300

    Develop (#8)
    * feat: 1_08-configure bundler
    * feat: add basic webpack settings
    * feat: add html-webpack-plugin and html template
    * feat: install styles plugins and loaders
    * feat: add styles compilation with hash
    * feat: add images loader
    * feat: add html loader
    * feat: add cleaning before each new compilation
    * feat: add font loader
    * feat: add svg-sprite-loader
    * feat: add module-alias plugin
    * feat: add post-css options and edit assets output name
    * WIP: draft project description as per R
    * WIP: draft project description as per RSS-ECOMM-1_17
    * WIP: draft script description as per RSS-ECOMM-1_19
    * WIP: update scripts documentation and provide instructions for running project locally
    * feat: 1_C04-development environment configuration
    * feat: install dependencies and write ts configs
    * feat: configure eslint and prettier
    * fix: fix loading svg
    * feat: add lint-staged, husky abd validate-branch-name
    * fix: fix pre-push script
    * fix: fix pre-push script
    * fix: fix pre-push script
    * fix: fix hooks
    * feat: install jest
    * fix: remove eslint-airbnb-base
    * fix: fix upstream dependencies
    * feat: install identity-obj-proxy for styles in tests
    * feat: install jest-environment-jsdom
    * feat: add collecting coverage
    * feat: add fileMock for jest
    * docs: add comments for settings in jest.config
    * fix: remove div from index.html
    * fix: fix readme.md file as per team comments
    * feat: development scripts  (#6)
    * feat: create script for running ESLint
    * feat: create scripts for running prettier
    * fix: update husky hook and lint-staged script
    
    ---------
    
    Co-authored-by: ArishaSuperNyasha <126005088+ArishaSuperNyasha@users.noreply.github.com>
    Co-authored-by: Dmitry <89168093+Grammeri@users.noreply.github.com>

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    "airbnb-typescript-prettier"
+  ],
+  parserOptions: {
+    sourceType: "module",
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
+  plugins: [
+    "prettier"
+  ],
+  rules: {
+    "max-lines-per-function": ["error", 40],
+    
+    "import/no-unresolved": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "class-methods-use-this": "off"
+  },
+  settings: {
+  }
+}
