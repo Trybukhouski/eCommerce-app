0bc00abf84993dc9258b494c3d52da7f0812e718
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2024-05-12 15:11:36 -0500

    refactor: remove unnecessary comments

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,12 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export const clientCredentials = {
-  clientId: process.env['CLIENT_ID'], // Используйте скобочную нотацию
-  clientSecret: process.env['CLIENT_SECRET'], // Используйте скобочную нотацию
-  scope: process.env['SCOPE'], // Используйте скобочную нотацию
-  apiUrl: process.env['API_URL'], // Используйте скобочную нотацию
-  authUrl: process.env['AUTH_URL'], // Используйте скобочную нотацию
-  projectKey: process.env['PROJECT_KEY'], // Используйте скобочную нотацию
+  clientId: process.env['CLIENT_ID'],
+  clientSecret: process.env['CLIENT_SECRET'],
+  scope: process.env['SCOPE'],
+  apiUrl: process.env['API_URL'],
+  authUrl: process.env['AUTH_URL'],
+  projectKey: process.env['PROJECT_KEY'],
 };
