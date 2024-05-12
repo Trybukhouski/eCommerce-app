b28d916eb2b023b85d994c5d42cb7b6436c7489a
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2024-05-12 12:38:06 -0500

    feat: create notification on successful and erronious logining

--- src/modules/loginPage/index.ts
+++ src/modules/loginPage/index.ts
@@ -1,6 +1,5 @@
 import { handleResponse } from '@shared/utils/errorHandling';
 import { loginPageUI } from '@modules/loginPage/ui';
-import { LoginData } from '@modules/loginPage/interfaces';
 import { clientCredentials } from '../../config';
 
 class LoginPage {
@@ -13,28 +12,43 @@ class LoginPage {
   private passwordInput: HTMLInputElement | null = null;
 
   constructor() {
-    document.addEventListener('DOMContentLoaded', () => {
-      this.emailInput = this.elem.querySelector('input[name="email"]') as HTMLInputElement;
-      this.passwordInput = this.elem.querySelector('input[name="password"]') as HTMLInputElement;
-      loginPageUI.form.addEventListener('submit', this.handleLogin.bind(this));
-    });
+    document.addEventListener('DOMContentLoaded', this.initialize.bind(this));
+  }
+
+  private initialize(): void {
+    this.emailInput = this.elem.querySelector('input[name="email"]');
+    this.passwordInput = this.elem.querySelector('input[name="password"]');
+
+    if (!this.emailInput || !(this.emailInput instanceof HTMLInputElement)) {
+      console.error('Email input not found or is not an input element.');
+      return;
+    }
+
+    if (!this.passwordInput || !(this.passwordInput instanceof HTMLInputElement)) {
+      console.error('Password input not found or is not an input element.');
+      return;
+    }
+
+    const { form } = this.uiApi;
+    if (form) {
+      form.addEventListener('submit', this.handleLogin.bind(this));
+    } else {
+      console.error('Form not found.');
+    }
   }
 
   private async handleLogin(event: Event): Promise<void> {
     event.preventDefault();
-    if (!this.emailInput || !this.passwordInput) {
+    if (!(this.emailInput && this.passwordInput)) {
       console.error('Email or password input not found');
       return;
     }
-    const loginData: LoginData = {
-      email: this.emailInput.value,
-      password: this.passwordInput.value,
-    };
+
     const url = `${clientCredentials.authUrl}/oauth/ecommerce2024/customers/token`;
     const body = new URLSearchParams({
       grant_type: 'password',
-      username: loginData.email,
-      password: loginData.password,
+      username: this.emailInput.value,
+      password: this.passwordInput.value,
     }).toString();
 
     try {
@@ -50,15 +64,35 @@ class LoginPage {
       });
       const data = await handleResponse(response);
       console.log('Authentication successful:', data);
-      alert('Logged in successfully!');
-      window.location.href = '/main.html';
+      this.showSuccess('Logged in successfully!');
+      setTimeout(() => {
+        window.location.href = '/main.html';
+      }, 2000);
     } catch (error) {
+      console.error('Login failed:', error);
       if (error instanceof Error) {
-        alert(error.message);
-      } else {
-        console.error('Unexpected error', error);
+        this.showError(error.message);
       }
     }
+  }
+
+  private showError(message: string) {
+    this.uiApi.emailError.textContent = message;
+    this.uiApi.passwordError.textContent = '';
+    this.emailInput!.classList.add('input-error');
+    this.passwordInput!.classList.add('input-error');
+
+    setTimeout(() => {
+      this.uiApi.emailError.textContent = '';
+      this.uiApi.passwordError.textContent = '';
+      this.emailInput!.classList.remove('input-error');
+      this.passwordInput!.classList.remove('input-error');
+    }, 5000);
+  }
+
+  private showSuccess(message: string) {
+    this.uiApi.successMessage.textContent = message;
+    this.uiApi.successMessage.style.display = 'block';
   }
 }
 export default LoginPage;
--- src/modules/shared/components/pageWithForm/page.ts
+++ src/modules/shared/components/pageWithForm/page.ts
@@ -6,29 +6,51 @@ class FormPageUI {
 
   public container: HTMLDivElement;
 
+  public messageContainer: HTMLDivElement;
+
   public inputArr: Input[];
 
   public submitButton: HTMLButtonElement;
 
   public form: HTMLFormElement;
 
+  public emailError: HTMLDivElement;
+
+  public passwordError: HTMLDivElement;
+
+  public successMessage: HTMLDivElement;
+
   constructor(formOptions: FormOptions, headerText: string) {
     this.section = document.createElement('section');
     this.container = document.createElement('div');
+    this.messageContainer = document.createElement('div');
+    this.messageContainer.className = style['message-container'];
 
     const header = document.createElement('h2');
     header.textContent = headerText;
     this.section.append(header);
 
     const form = new Form(formOptions);
-
     this.inputArr = form.inputArr;
     this.submitButton = form.button;
     this.form = form.form;
 
-    this.section.append(this.container);
-    this.container.append(this.form);
+    this.emailError = document.createElement('div');
+    this.emailError.id = 'email-error';
+    this.emailError.className = style['error-message'];
 
+    this.passwordError = document.createElement('div');
+    this.passwordError.id = 'password-error';
+    this.passwordError.className = style['error-message'];
+
+    this.successMessage = document.createElement('div');
+    this.successMessage.id = 'success-message';
+    this.successMessage.className = style['success-message'];
+
+    this.messageContainer.append(this.emailError, this.passwordError, this.successMessage);
+    this.container.append(this.messageContainer, this.form);
+
+    this.section.append(this.container);
     this.section.classList.add(style['login-page']);
   }
 }
