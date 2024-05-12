0354fd240a9e9bfed0807eac6f966a1886653e18
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2024-05-11 19:15:00 -0500

    feat: implement .env

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const clientCredentials = {
-  clientId: 'P7AHNZvx7l7ATvqkj10zbEQa',
-  clientSecret: 'z7kCZZoMPb5UmBBl_0g70ROn-vEmvGLn',
-  scope:
-    'introspect_oauth_tokens:ecommerce2024 manage_my_profile:ecommerce2024 manage_api_clients:ecommerce2024 manage_customers:ecommerce2024 create_anonymous_token:ecommerce2024',
-  apiUrl: 'https://api.us-central1.gcp.commercetools.com',
-  authUrl: 'https://auth.us-central1.gcp.commercetools.com',
+  clientId: process.env['CLIENT_ID'], // Используйте скобочную нотацию
+  clientSecret: process.env['CLIENT_SECRET'], // Используйте скобочную нотацию
+  scope: process.env['SCOPE'], // Используйте скобочную нотацию
+  apiUrl: process.env['API_URL'], // Используйте скобочную нотацию
+  authUrl: process.env['AUTH_URL'], // Используйте скобочную нотацию
+  projectKey: process.env['PROJECT_KEY'], // Используйте скобочную нотацию
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
 let mode = 'development';
 if (process.env.NODE_ENV === 'production') {
@@ -11,6 +12,15 @@ module.exports = {
   entry: './src/index.ts',
   mode: mode,
   resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify"),
+      "vm": require.resolve("vm-browserify"),
+      "buffer": require.resolve("buffer/"),
+      "stream": require.resolve("stream-browserify"),
+      process: require.resolve('process/browser'),
+    },
     extensions: ['.tsx', '.ts', '.js'],
     alias: {
       "@shared": path.resolve(__dirname, 'src/shared/'),
@@ -31,6 +41,17 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css'
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.CLIENT_ID': JSON.stringify(process.env.CLIENT_ID || 'P7AHNZvx7l7ATvqkj10zbEQa'),
+      'process.env.CLIENT_SECRET': JSON.stringify(process.env.CLIENT_SECRET || 'z7kCZZoMPb5UmBBl_0g70ROn-vEmvGLn'),
+      'process.env.SCOPE': JSON.stringify(process.env.SCOPE || 'introspect_oauth_tokens:ecommerce2024 manage_my_profile:ecommerce2024 manage_api_clients:ecommerce2024 manage_customers:ecommerce2024 create_anonymous_token:ecommerce2024'),
+      'process.env.API_URL': JSON.stringify(process.env.API_URL || 'https://api.us-central1.gcp.commercetools.com'),
+      'process.env.AUTH_URL': JSON.stringify(process.env.AUTH_URL || 'https://auth.us-central1.gcp.commercetools.com'),
+      'process.env.PROJECT_KEY': JSON.stringify(process.env.PROJECT_KEY || 'ecommerce2024'),
     }),
   ],
   module: {
@@ -66,7 +87,7 @@ module.exports = {
                       [
                         'postcss-preset-env',
                         {
-                          stage: 3, 
+                          stage: 3,
                           features: {
                             'custom-properties': false,
                           },
