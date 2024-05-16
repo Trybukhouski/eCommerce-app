408681cdc51f6a1c2449711361504aa98c4e133f
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-05-16 14:37:04 +0300

    fix: add Input types types

--- src/shared/components/input/config.ts
+++ src/shared/components/input/config.ts
@@ -1,6 +1,30 @@
+type InputTypes =
+  | 'button'
+  | 'checkbox'
+  | 'color'
+  | 'date'
+  | 'datetime-local'
+  | 'email'
+  | 'file'
+  | 'hidden'
+  | 'image'
+  | 'month'
+  | 'number'
+  | 'password'
+  | 'radio'
+  | 'range'
+  | 'reset'
+  | 'search'
+  | 'submit'
+  | 'tel'
+  | 'text'
+  | 'time'
+  | 'url'
+  | 'week';
+
 interface InputOptions {
   name?: string;
-  type?: string;
+  type?: InputTypes;
   placeholder?: string;
   labelText?: string;
   value?: string;
