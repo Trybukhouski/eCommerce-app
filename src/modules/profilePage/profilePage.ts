3afc27e0ca785a96cc2a5e09dd30afd2590b6444
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-05-25 20:52:36 +0300

    refactor: reduce addEditClickListener method

--- src/modules/profilePage/profilePage.ts
+++ src/modules/profilePage/profilePage.ts
@@ -17,20 +17,15 @@ export class ProfilePage {
       const { target } = event;
       const button = (target as HTMLElement | null)?.closest('button');
       const isEditButton = button?.classList.contains(this.uiApi.editButtonClassName);
-      if (!target || !button || !isEditButton) {
-        return;
-      }
-
       const formEntry = Object.entries(this.uiApi.forms).find(
         (entry) => entry[1].editButton === button
       );
-      if (!formEntry) {
+      const formKey = formTypes.find((k) => (formEntry ? formEntry[0] === k : false));
+
+      if (!target || !button || !isEditButton || !formEntry || !formKey) {
         return;
       }
-      const formKey = formTypes.find((k) => formEntry[0] === k);
-      if (!formKey) {
-        return;
-      }
+
       const isDisabled = this.uiApi.forms[formKey].form.fieldsetElement?.disabled;
       this.uiApi.changeRequired(formKey, !!isDisabled);
       this.uiApi.disableFieldset(formKey, !isDisabled);
