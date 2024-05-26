d2d5b37f81e1e5353ba9c347f11fd4822e010355
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-05-26 16:06:57 +0300

    feat: add styles and decorative elements

--- src/modules/profilePage/ui/profilePageUi.ts
+++ src/modules/profilePage/ui/profilePageUi.ts
@@ -1,17 +1,74 @@
-import './style.scss';
 import editIcon from '@assets/sprites/edit/edit-clipboard.svg';
 import { Form, Button } from '@shared';
+import * as style from './style.module.scss';
 import { profilePageOptions } from './config';
 
 type KeyOfProfileForms = keyof typeof profilePageOptions;
 const formTypes: KeyOfProfileForms[] = ['basic', 'password', 'delivery', 'bills'];
 
 class ProfilePageUI {
+  public section: HTMLElement;
+
   public elem: HTMLElement;
 
   public editButtonClassName = 'edit-form';
 
-  private createEditButtonProto() {
+  private editButtonProto = this.createEditButtonProto();
+
+  public forms = {
+    basic: {
+      form: new Form(profilePageOptions.basic),
+      editButton: this.editButtonProto.cloneNode(true),
+      legendText: 'Profile information',
+    },
+    password: {
+      form: new Form(profilePageOptions.password),
+      editButton: this.editButtonProto.cloneNode(true),
+      legendText: 'Password',
+    },
+    delivery: {
+      form: new Form(profilePageOptions.delivery),
+      editButton: this.editButtonProto.cloneNode(true),
+      legendText: 'Delivery address',
+    },
+    bills: {
+      form: new Form(profilePageOptions.bills),
+      editButton: this.editButtonProto.cloneNode(true),
+      legendText: 'Bills address',
+    },
+  };
+
+  constructor() {
+    const section = document.createElement('section');
+    section.classList.add(style['profile']);
+    this.section = section;
+    this.elem = section;
+    const formElements = formTypes.map((k) => this.forms[k].form.form);
+    this.elem.append(...formElements);
+
+    this.setInitialFormsSettings();
+    this.addDecorativeElements();
+  }
+
+  private addDecorativeElements(): void {
+    const heading = document.createElement('h2');
+    heading.textContent = 'Profile';
+    this.section.prepend(heading);
+
+    formTypes.forEach((key) => {
+      const form = this.forms[key];
+      const { legendText } = form;
+      const fieldset = form.form.fieldsetElement;
+      if (!fieldset) {
+        return;
+      }
+      const legend = document.createElement('legend');
+      legend.textContent = legendText;
+      fieldset.prepend(legend);
+    });
+  }
+
+  private createEditButtonProto(): HTMLButtonElement {
     const { button } = new Button({
       className: 'edit-icon',
       type: 'button',
@@ -22,36 +79,6 @@ class ProfilePageUI {
     });
     button.classList.add(this.editButtonClassName);
     return button;
-  }
-
-  private editButtonProto = this.createEditButtonProto();
-
-  public forms = {
-    basic: {
-      form: new Form(profilePageOptions.basic),
-      editButton: this.editButtonProto.cloneNode(true),
-    },
-    password: {
-      form: new Form(profilePageOptions.password),
-      editButton: this.editButtonProto.cloneNode(true),
-    },
-    delivery: {
-      form: new Form(profilePageOptions.delivery),
-      editButton: this.editButtonProto.cloneNode(true),
-    },
-    bills: {
-      form: new Form(profilePageOptions.bills),
-      editButton: this.editButtonProto.cloneNode(true),
-    },
-  };
-
-  constructor() {
-    const section = document.createElement('section');
-    this.elem = section;
-    const formElements = formTypes.map((k) => this.forms[k].form.form);
-    this.elem.append(...formElements);
-
-    this.setInitialFormsSettings();
   }
 
   private setInitialFormsSettings(): void {
